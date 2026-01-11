f34504686a5c323cbf01b588d9440fb58fecb0dd
Kuldas <jsem@stepankulik.cz>
2026-01-11 19:28:46 +0100

    PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,43 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-
+import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite';
 import pkg from './package.json';
 
 export default defineConfig({
   root: '.',
   base: '/',
-  plugins: [tailwindcss()],
+  plugins: [
+    tailwindcss(), 
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        lang: 'cs',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      // devOptions: {
+      //   enabled: true
+      // } 
+    })
+  ],
   define: {
     'import.meta.env.VITE_APP_VERSION': JSON.stringify(pkg.version),
   },
