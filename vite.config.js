e2fc9c3c0156eed94122011f45f137c8fde534c5
Kuldas <jsem@stepankulik.cz>
2026-01-12 00:10:57 +0100

    refactor: remove PWA plugin and related configurations from vite.config.js; update package.json to remove vite-plugin-pwa dependency

--- vite.config.js
+++ vite.config.js
@@ -7,33 +7,7 @@ import pkg from './package.json';
 export default defineConfig({
   base: './',
   plugins: [
-    tailwindcss(), 
-    VitePWA({ 
-      manifest: {
-        name: 'My Awesome App',
-        short_name: 'MyApp',
-        description: 'My Awesome App description',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#ffffff',
-        lang: 'cs',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      },
-      // devOptions: {
-      //   enabled: true
-      // } 
-    })
+    tailwindcss(),
   ],
   define: {
     'import.meta.env.VITE_APP_VERSION': JSON.stringify(pkg.version),
